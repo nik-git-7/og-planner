d0c1eb81d9f1ed68cbabc9d58b3a3cf390a1712f
semperErro <simon_paul58@web.de>
2020-11-09 19:02:24 +0100

    stripped down cli-config.php

--- public/cli-config.php
+++ public/cli-config.php
@@ -2,10 +2,6 @@
 
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
 
-// replace with file to your own project bootstrap
 require_once 'bootstrap.php';
 
-// replace with mechanism to retrieve EntityManager in your app
-$entityManager = getEntityManager();
-
-return ConsoleRunner::createHelperSet($entityManager);
+return ConsoleRunner::createHelperSet(getEntityManager());
