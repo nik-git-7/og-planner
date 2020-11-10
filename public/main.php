0f09b3bd59037bc7bed37bde0c57a717f876b98f
niklas <nskf2000@gmail.com>
2020-11-10 19:08:02 +0100

    Refactored main.php

--- public/main.php
+++ public/main.php
@@ -6,6 +6,7 @@ use Doctrine\ORM\EntityManager;
 use ogPlanner\dao\ILessonRepo;
 use ogPlanner\dao\IUserCourseTimetableConnectorRepo;
 use ogPlanner\dao\IUserRepo;
+use ogPlanner\dao\LessonRepo;
 use ogPlanner\model\IEntry;
 use ogPlanner\model\ILesson;
 use ogPlanner\model\IUser;
@@ -18,85 +19,117 @@ use ogPlanner\utils\OGScraper;
 use ogPlanner\utils\TableScraper;
 use ogPlanner\utils\Util;
 
-
-function withLogger(string $msg, $fun): void
+class Main
 {
-    $code = $fun();
-    Util::logToFile(sprintf($msg, $code));
-    echo sprintf($msg, $code);
-}
+    public static function withLogger(string $msg, $fun): void
+    {
+        $code = $fun();
+        Util::logToFile(sprintf($msg, $code));
+        echo sprintf($msg, $code);
+    }
 
-function main(): int
-{
-    /** @var EntityManager $entityManager */
-    /** @var IUserCourseTimetableConnectorRepo $connectorRepo */
-    /** @var ILessonRepo $lessonRepo */
-    /** @var IUserRepo $userRepo */
-    $entityManager = Config::getEntityManager();
-    $userRepo = $entityManager->getRepository(User::class);
-    $connectorRepo = $entityManager->getRepository(UserCourseTimetableConnector::class);
-    $lessonRepo = $entityManager->getRepository(Lesson::class);
-    $users = $userRepo->findAll();
+    public static function run(): array
+    {
+        // region scraping
+        $ogScraper = new OGScraper(Config::PLANNER_URL);
+        $ogScraperData = $ogScraper->scrape();
 
-    $tableScraper = new TableScraper(Config::PLANNER_URL);
-    $table = $tableScraper->scrape();
-    $ogScraper = new OGScraper(Config::PLANNER_URL);
-    $ogScraperData = $ogScraper->scrape();
 
-    $dateParsed = date_parse($ogScraperData['plan_date']);
-    $dateStr = "{$dateParsed['day']}.{$dateParsed['month']}.{$dateParsed['year']}";
-    $planDayOfWeek = date('N', strtotime($dateStr)) - 1;
+        if (!Util::updateFileContents($ogScraperData['plan_update'], Config::LAST_UPDATE)) {
+            return [];
+        }
 
-    $courseMap = Util::convertTableToMap($table);
+        $tableScraper = new TableScraper(Config::PLANNER_URL);
+        $table = $tableScraper->scrape();
+        if ($table->isEmpty()) {
+            return [];
+        }
 
-    /** @var IUser $user */
-    foreach ($users as $user) {
-        $emailEntries = [];
-        $uctcs = $connectorRepo->findByUserId($user->getId());
+        // endregion
 
-        /** @var IUserCourseTimetableConnector $uctc */
-        foreach ($uctcs as $uctc) {
-            $course = $uctc->getCourse();
-            if (!array_key_exists($course, $courseMap)) {
+        $courseMap = Util::convertTableToMap($table);
+
+        $dateParsed = date_parse($ogScraperData['plan_date']);
+        $dateStr = "{$dateParsed['day']}.{$dateParsed['month']}.{$dateParsed['year']}";
+        $planDayOfWeek = date('N', strtotime($dateStr)) - 1;
+
+        $resultUserEntryDayMap = [];
+
+        // region get user and connector repos
+        $entityManager = Config::getEntityManager();
+        $userRepo = $entityManager->getRepository(User::class);
+
+        /** @var IUserCourseTimetableConnectorRepo $connectorRepo */
+        $connectorRepo = $entityManager->getRepository(UserCourseTimetableConnector::class);
+
+        /** @var ILessonRepo $lessonRepo */
+        $lessonRepo = $entityManager->getRepository(Lesson::class);
+
+        /** @var IUserRepo $userRepo */
+        $users = $userRepo->findAll();
+
+        // endregion
+
+        /** @var IUser $user */
+        foreach ($users as $user) {
+            $emailEntries = [];
+            $uctcs = $connectorRepo->findByUserId($user->getId());
+
+            /** @var IUserCourseTimetableConnector $uctc */
+            foreach ($uctcs as $uctc) {
+                $course = $uctc->getCourse();
+                if (!array_key_exists($course, $courseMap)) {
+                    continue;
+                }
+                $entries = $courseMap[$course];
+                $emailEntries = self::getEMailEntries($entries, $planDayOfWeek, $lessonRepo, $uctc);
+            }
+
+            if (count($emailEntries) == 0) {
                 continue;
             }
 
-            $entries = $courseMap[$course];
-            if ($uctc->getTimetableId() == 0) {    // User is not a student of Oberstufe
-                $emailEntries = $entries;
-            } else {    // User is a student of Oberstufe
-                $dayLessons = $lessonRepo->findByTimetableIdAndDay($uctc->getTimetableId(), $planDayOfWeek);
+            $resultUserEntryDayMap[] = ['user' => $user, 'email_entries' => $emailEntries,
+                'plan_day_of_week' => $planDayOfWeek];
 
-                /** @var ILesson $dayLesson */
-                foreach ($dayLessons as $dayLesson) {
-                    /** @var IEntry $entry */
-                    foreach ($entries as $entry) {
-                        if ($dayLesson->getPosition() == $entry->getPosition() &&
-                            $dayLesson->getSubject() == $entry->getSubject()) {
-                            $emailEntries[] = $entry;
-                        }
+            /*if (OGMailer::sendEntryMail($user, $emailEntries, $planDayOfWeek)) {
+                Util::logToFile('Successfully sent E-Mail to #' . $user->getId() . ' - ' . $user->getName() .
+                    ' with E-Mail ' . $user->getEmail());
+            } else {
+                Util::logToFile('Could not send E-Mail to #' . $user->getId() . ' - ' . $user->getName() .
+                    ' with E-Mail ' . $user->getEmail());
+            }*/
+        }
+
+        return $resultUserEntryDayMap;
+    }
+
+    private static function getEMailEntries(array $entries, int $planDayOfWeek, ILessonRepo $lessonRepo,
+                                            IUserCourseTimetableConnector $uctc): array
+    {
+        $emailEntries = [];
+
+        if ($uctc->getTimetableId() == 0) {    // User is not a student of Oberstufe
+            $emailEntries = $entries;
+        } else {    // User is a student of Oberstufe
+            $dayLessons = $lessonRepo->findByTimetableIdAndDay($uctc->getTimetableId(), $planDayOfWeek);
+
+            /** @var ILesson $dayLesson */
+            foreach ($dayLessons as $dayLesson) {
+                /** @var IEntry $entry */
+                foreach ($entries as $entry) {
+                    if ($dayLesson->getPosition() == $entry->getPosition() &&
+                        $dayLesson->getSubject() == $entry->getSubject()) {
+                        $emailEntries[] = $entry;
+                        break;
                     }
                 }
             }
         }
-        // lessons(id, timetableId, day, position, subject)
-
-        if (count($emailEntries) == 0) {
-            continue;
-        }
-
-        if (OGMailer::sendEntryMail($user, $emailEntries, $planDayOfWeek)) {
-            Util::logToFile('Successfully sent E-Mail to #' . $user->getId() . ' - ' . $user->getName() .
-                ' with E-Mail ' . $user->getEmail());
-        } else {
-            Util::logToFile('Could not send E-Mail to #' . $user->getId() . ' - ' . $user->getName() .
-                ' with E-Mail ' . $user->getEmail());
-        }
+        return $emailEntries;
     }
-
-    return Config::EXIT_SUCCESS;
 }
 
-withLogger('Executed with Code: %d', function (): int {
-    return main();
-});
+/*Main::withLogger('Executed with Code: %d', function (): array {
+    return Main::run();
+});*/
